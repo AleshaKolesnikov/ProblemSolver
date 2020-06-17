dde8e814fca5c204f34c1db56bfd0a80cc8c02b5
Lex Kolesnikov <kyposani@outlook.com>
2020-06-17 17:49:14 +0300

    task 2 solved

---
+++ ArrayTasks/Array_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array_2
+{
+    // Given an array [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7]. 
+    // Numbers can be positive and negative. 
+    // Dysplay only negative numbers
+    // which are greater than -10, but less than -3.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7 };
+
+            foreach (var i in array)
+            {
+                if (i > - 10 && i < -3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
