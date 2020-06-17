0de58bc8a518025f15e8d44bd83d0d3c5d69e398
Lex Kolesnikov <kyposani@outlook.com>
2020-06-17 17:36:51 +0300

    solved

---
+++ ArrayTasks/Array_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_1
+{
+    // Outputting an array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
