1bc16b391579bf10b7f49b3855d80bec45d3cc5d
Lex Kolesnikov <kyposani@outlook.com>
2020-06-17 18:30:14 +0300

    task 4 solved

---
+++ ArrayTasks/Array_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_4
+{
+    // Дан массив числами(строчного типа), 
+    // например: ["10", "20", "30", "50", "235", "3000"]. 
+    // Выведите на экран только те числа из массива, которые начинаются на цифру 1, 2 или 5.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "10", "20", "30", "50", "235", "3000" };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i][0].Equals('1') || array[i][0].Equals('2') || array[i][0].Equals('5'))
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
