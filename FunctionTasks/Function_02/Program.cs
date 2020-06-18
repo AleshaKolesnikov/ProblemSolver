b20045ecf145e8237ca33f7f70a0eeadd22c6671
Lex Kolesnikov <kyposani@outlook.com>
2020-06-18 19:22:03 +0300

    task 02 solved

---
+++ FunctionTasks/Function_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Function_02
+{
+    // Сделайте функцию, которая возвращает куб числа и его квадрат. Число передается параметром
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Square(9));
+            Console.WriteLine(Cube(5));
+        }
+
+        public static int Square (int num)
+        {
+            return num * num;
+        }
+
+        public static int Cube (int num)
+        {
+            return num * num * num;
+        }
+    }
+}
