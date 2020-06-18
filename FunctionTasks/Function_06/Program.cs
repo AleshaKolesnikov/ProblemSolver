cb31ce5b47cc0630a2f75a8397aa132035ff4a79
Lex Kolesnikov <kyposani@outlook.com>
2020-06-18 19:58:50 +0300

    work, but not well

---
+++ FunctionTasks/Function_06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Function_06
+{
+    //Напишите ф-цию, в которую передается массив с целыми числами. Верните новый
+    //массив, где останутся лежать только четные из этих чисел.Для этого используйте
+    //вспомогательную функцию isEven из предыдущей задачи.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = OnlyEven(new int []{ 12, 14, 16, 10, 9, 5, 12 });
+
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int[] OnlyEven(int [] array)
+        {
+            int length = array.Length;
+            int[] onlyEvenArray = new int[length];
+
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (IsEven(array[i]))
+                {
+                    onlyEvenArray[j] = array[i];
+                    j++;
+                }
+                else
+                    length--;
+            }
+            return onlyEvenArray;
+        }
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
