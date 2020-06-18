7b59e134d8a66a7853e9266249074a5f16a67bd8
Lex Kolesnikov <kyposani@outlook.com>
2020-06-18 19:28:20 +0300

    task 03 solved

---
+++ FunctionTasks/Function_03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Function_03
+{
+    // Напишите функции min и max, которые возвращают меньшее и большее из чисел a и b.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Max(1, 2);
+            Min(5, 6);
+
+            Max(10, 10);
+            Min(15, 15);
+        }
+        public static void Max(int num1, int num2)
+        {
+            if (num1 > num2)
+                Console.WriteLine(num1);
+            else if (num2 > num1)
+                Console.WriteLine(num2);
+            else
+                Console.WriteLine("Equals");
+        }
+        public static void Min(int num1, int num2)
+        {
+            if (num1 < num2)
+                Console.WriteLine(num1);
+            else if (num2 < num1)
+                Console.WriteLine(num2);
+            else
+                Console.WriteLine("Equals");
+        }
+    }
+}
