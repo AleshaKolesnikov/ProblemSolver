9a89f300c0759bbf1a8564a75ff3983231487bfa
Lex Kolesnikov <kyposani@outlook.com>
2020-06-18 19:38:12 +0300

    task 05 solved

---
+++ FunctionTasks/Function_05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Function_05
+{
+    // Сделайте функцию isEven() (even - это четный), которая параметром принимает целое
+    // число и проверяет: четное оно или нет.Если четное - пусть функция возвращает true,
+    // если нечетное - false.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsEven(9)); 
+        }
+
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
