1d158aab2a140dc939cfe6a0dccf103180e8b6d6
Lex Kolesnikov <kyposani@outlook.com>
2020-06-18 19:34:39 +0300

    task 04 solved

---
+++ FunctionTasks/Function_04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Function_04
+{
+    // Напишите две функции: первая ф-ция должна возвращать массив с числовыми
+    // значениями, диапазон которых должен вводиться пользователем с клавиатуры;
+    // вторая – выводить полученный массив.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = GetArray();
+            ShowArray(result);
+        }
+
+        public static int[] GetArray()
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = { num1, num2, num3 };
+            return array;
+        }
+
+        public static void ShowArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
