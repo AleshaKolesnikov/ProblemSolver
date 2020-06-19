d170866ac2f23a5323a27de42fa4a3ef23cb9734
Lex Kolesnikov <kyposani@outlook.com>
2020-06-19 17:46:45 +0300

    task 09a solved

---
+++ FunctionTasks/Function_09a/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Function_09a
+{
+    // Дан массив с числами (передается параметром). Выведите последовательно его
+    // элементы, используя рекурсию и не используя цикл.
+    class Program
+    {
+        public static int iterator = 0;
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Output(array);
+
+        }
+        static void Output(int[] array)
+        {
+            if (iterator < array.Length)
+            {
+                Console.WriteLine(array[iterator]);
+                iterator++;
+                Output(array);
+            }
+            else return;
+            
+        }
+    }
+}
