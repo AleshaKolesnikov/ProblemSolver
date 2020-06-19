8c1a87132bbd9cee35c3489bb960a9d35240f272
Lex Kolesnikov <kyposani@outlook.com>
2020-06-19 17:06:26 +0300

    task 09 solved

---
+++ FunctionTasks/Function_09/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Function_09
+{
+    class Program
+    {
+        // Дано число. Сложите его цифры. Если сумма получилась более 9-ти, опять сложите его цифры.
+        // И так, пока сумма не станет однозначным числом(9 и менее). Исп.рекурсию.
+        static void Main(string[] args)
+        {
+            int num = 99;
+            int result = Result(num);
+
+            Console.WriteLine(result); 
+        }
+
+        static int FindDozens(int num)
+        {
+                return num / 10;
+        }
+
+        static int FindUnits(int num)
+        {
+            
+                return num % 10;
+        }
+
+        static int Result(int result)
+        {
+            int dozens = FindDozens(result);
+            int units = FindUnits(result);
+
+            if (dozens.Equals(0))
+            {
+                return units;
+            }
+            else
+            {
+                result = dozens + units;
+                return Result(result);
+
+            }
+        }
+    }
+}
