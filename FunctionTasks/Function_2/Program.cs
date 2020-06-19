187d9742a21bd2af1734f0285a2b96f253ee7d67
Lex Kolesnikov <kyposani@outlook.com>
2020-06-19 22:00:55 +0300

    task 2 solved

--- FunctionTasks/Function_2/Program.cs
+++ FunctionTasks/Function_2/Program.cs
@@ -13,39 +13,80 @@ namespace Function_2
     class Program
     {
         public static int _height;
+        public static int _input;
+        public static char[] array;
+        public static char symbol;
         static void Main(string[] args)
         {
             Console.WriteLine("Input the height of trianlge");
-            _height = Convert.ToInt32(Console.ReadLine());
+            _input = Convert.ToInt32(Console.ReadLine());
+            _height = _input;
+            symbol = '*';
 
-            char[] array = new char[_height * 2];
-            int incr = _height;
-            char symbol = '*';
-
-            UnturnedTriangle(array, symbol, incr);
-
+            DrawTriangle(array);
+            DrawTurned(array);
         }
-
-        public static void UnturnedTriangle(char[] array, char symbol, int incr)
+        public static void DrawTriangle(char[] array)
         {
+            array = new char[(_height * 2)];
             Console.WriteLine("Unturned triangle:");
-            do
-            {
-                for (int i = 0; i < array.Length; i++)
-                {
-                    array[_height - 1] = symbol;
-                    Console.Write($"{array[i]}");
-                }
-                Console.WriteLine();
-                if (_height >= 0)
-                {
-                    incr++;
-                    array[incr - 1] = symbol;
-                    _height--;
-                }
-            }
-            while (_height > 0);
-        }
 
+            int next = _height;
+
+            for (; _height > 0; next++, _height--)
+            {
+                FillFromMiddle(array, symbol);
+                Output(array);
+                array[next] = symbol;
+            }
+            _height = _input;
+        }
+        public static void DrawTurned(char[] array)
+        {
+            array = new char[(_height * 2) + 1];
+            Console.WriteLine("Turned:");
+
+            int incrInd = 0;
+            int decrInd = array.Length - 1;
+            char[] tempArray = new char[array.Length];
+            char space = ' ';
+
+            FillArray(array, symbol);
+
+            for (; _height > 0; _height--)
+            {
+                for (int i = 0; i < 1; incrInd++, decrInd--, i++)
+                {
+                    array[incrInd] = space;
+                    array[decrInd] = space;
+                }
+                tempArray = array;
+                Output(tempArray);
+            }
+            _height = _input;
+        }
+        public static void Output(char[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void FillArray(char[] array, char symbol)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = symbol;
+            }
+        }
+        public static void FillFromMiddle(char [] array, char symbol)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[_height - 1] = symbol;
+            }
+        }
     }
+
 }
